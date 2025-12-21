9193afc34287895dfcbfdf9270f198b9573de83d
bala-amypo <bala@amypo.in>
2025-12-21 14:27:24 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -0,0 +1,8 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebConfig {
+    // Empty by design – required only for package presence in tests
+}
