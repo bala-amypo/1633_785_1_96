d1069ec9d6b8af313c30ee01215ba33d9598f3ad
bala-amypo <bala@amypo.in>
2025-12-21 11:41:06 +0000

    User commit

---
+++ src/main/java/com/example/demo/model/LeaveRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.model;
+
+import jakarta.persistence.*;
+import java.time.LocalDate;
+
+@Entity
+public class LeaveRequest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String employee;
+    private LocalDate date;
+}
