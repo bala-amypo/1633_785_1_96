fbf5215546c2ff8330f244f5a321db0d7a73840a
bala-amypo <bala@amypo.in>
2025-12-22 08:08:58 +0000

    User commit

--- src/main/java/com/example/demo/exception/UnauthorizedException.java
+++ src/main/java/com/example/demo/exception/UnauthorizedException.java
@@ -10,3 +10,15 @@ public class UnauthorizedException extends RuntimeException {
         super(message);
     }
 }
+package com.example.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.UNAUTHORIZED)
+public class UnauthorizedException extends RuntimeException {
+
+    public UnauthorizedException(String message) {
+        super(message);
+    }
+}
