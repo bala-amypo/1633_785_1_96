7a54a6e8b5277a1f4eae7549f8bce7c982552192
bala-amypo <bala@amypo.in>
2025-12-26 10:32:32 +0000

    User commit

---
+++ src/main/java/com/example/demo/util/Util.java
@@ -0,0 +1,15 @@
+package com.example.demo.util;
+
+import java.time.LocalDate;
+import java.util.*;
+
+public class DateRangeUtil {
+    public static List<LocalDate> daysBetween(LocalDate start, LocalDate end) {
+        List<LocalDate> list = new ArrayList<>();
+        while (!start.isAfter(end)) {
+            list.add(start);
+            start = start.plusDays(1);
+        }
+        return list;
+    }
+}
