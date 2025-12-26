967b47f023a9bd8e1dd652c42a91fa14e1e1c2f5
bala-amypo <bala@amypo.in>
2025-12-26 14:07:31 +0000

    User commit

--- src/main/java/com/example/demo/repository/TeamCapacityConfigRepository.java
+++ src/main/java/com/example/demo/repository/TeamCapacityConfigRepository.java
@@ -1,9 +1,14 @@
 package com.example.demo.repository;
 
-import com.example.demo.model.*;
-import java.time.LocalDate;
-import java.util.*;
-public interface TeamCapacityConfigRepository {
-    Optional<TeamCapacityConfig> findByTeamName(String team);
-    TeamCapacityConfig save(TeamCapacityConfig c);
+import com.example.demo.model.TeamCapacityConfig;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface TeamCapacityConfigRepository
+        extends JpaRepository<TeamCapacityConfig, Long> {
+
+    Optional<TeamCapacityConfig> findByTeamName(String teamName);
 }
