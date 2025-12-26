6e86f109947e2d07900040099532c4ea476b35d0
bala-amypo <bala@amypo.in>
2025-12-26 17:21:45 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AuthServiceImpl.java
@@ -1,3 +1,13 @@
-@Override
-public void login() {
+package com.example.demo.service.impl;
+
+import com.example.demo.service.AuthService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthServiceImpl implements AuthService {
+
+    @Override
+    public void login() {
+        // TODO: implement login logic later
+    }
 }
