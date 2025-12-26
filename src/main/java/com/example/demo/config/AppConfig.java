da6719c1d79febf3e1ec5d3b5936dfb40f8891ac
bala-amypo <bala@amypo.in>
2025-12-26 13:13:31 +0000

    User commit

--- src/main/java/com/example/demo/config/AppConfig.java
+++ src/main/java/com/example/demo/config/AppConfig.java
@@ -3,12 +3,13 @@ package com.example.demo.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-
 @Configuration
 public class AppConfig {
 
     @Bean
-    public BCryptPasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
+    public AuthService authService(UserAccountRepository repo,
+                                   PasswordEncoder encoder,
+                                   JwtTokenProvider jwt) {
+        return new AuthServiceImpl(repo, encoder, jwt);
     }
 }
